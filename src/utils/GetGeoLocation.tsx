eb6f0638c64edc1bdbdf17d68243a60a3fbeb0f7
Mukesh Singh <mukeshsingh@codebrewinnovations.com>
2025-02-20 17:39:47 +0530

    fixes

--- src/utils/GetGeoLocation.tsx
+++ src/utils/GetGeoLocation.tsx
@@ -1,22 +1,23 @@
 
 export const GetGeoLoactions = () => {
   return new Promise((resolve, reject) => {
+    resolve({ latitude: "30.7333", longitude: "76.7794" });
     // Check if the browser supports Geolocation API
-    if (navigator.geolocation) {
-      // Get the current location
-    
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          resolve({ latitude, longitude });
-        },
-        (error) => {
-          reject(error.message);
-        }
-      );
-    } else {
-      reject('Geolocation is not supported by your browser');
-    }
+    // if (navigator.geolocation) {
+    //   // Get the current location
+
+    //   navigator.geolocation.getCurrentPosition(
+    //     (position) => {
+    //       const { latitude, longitude } = position.coords;
+    //       resolve({ latitude, longitude });
+    //     },
+    //     (error) => {
+    //       reject(error.message);
+    //     }
+    //   );
+    // } else {
+    //   reject('Geolocation is not supported by your browser');
+    // }
   });
 }
 
